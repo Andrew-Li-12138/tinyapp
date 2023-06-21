6a59f2467e4f81bb78bf9ad35fee3b9db6ff9df0
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-21 19:24:40 -0400

    add route to respond for end point /urls.json to send JSON data

--- express_server.js
+++ express_server.js
@@ -5,10 +5,14 @@ const PORT = 8080; // default port 8080
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-};
+}; // JSON data
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
