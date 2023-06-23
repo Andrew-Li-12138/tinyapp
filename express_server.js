43c81490dd66371037ec21dff41e494629bf8329
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-23 17:50:35 -0400

    added route sto render urls/news page, handle post request from /urls page, and redirecting short url to long url

--- express_server.js
+++ express_server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const { generateRandomString } = require('./supportFunctions');
-const randomString = generateRandomString(6);
 
-console.log(randomString);
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,7 +14,7 @@ app.get("/", (req, res) => {
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 }; // JSON data
 
 app.get("/urls.json", (req, res) => {
@@ -32,18 +30,31 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);// Log the POST request body to the console
-  res.send("Ok");// Respond with 'Ok' ( will replace this later)
-})
-
 app.get("/urls/new",(req, res) => {
   res.render("urls_new")
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);// Log the POST request body to the console
+  //add key value pair - randomString : newly entered long url - to urlDatabse
+  const longURL = req.body.longURL.trim(); //delete space if there is any before/after entered URL to avoid errors in future redirecting
+  const randomString = generateRandomString(6);
+  urlDatabase[randomString] = longURL;
+  console.log(urlDatabase);
+  //redirect to urls/:id after form submission
+  res.redirect(`/urls/${randomString}`);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+// when click at shortened url from /urls/:id page (/u/:id link), redirect to its longURL address
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  console.log(longURL)
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
