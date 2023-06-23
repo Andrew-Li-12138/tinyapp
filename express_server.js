41342f8747ef801a8d8e0b6cc2b269efa9f68b18
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-23 19:40:09 -0400

    add route to handle post request from urls/:id/delete and delete selected id(short url) and its pairing long url

--- express_server.js
+++ express_server.js
@@ -50,6 +50,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls')
+})
+
 // when click at shortened url from /urls/:id page (/u/:id link), redirect to its longURL address
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
