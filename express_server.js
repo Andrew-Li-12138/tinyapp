2f6031b2844c886b54174d0600402f2a4ced36d9
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-24 19:47:58 -0400

    modified routes and passed req.cookie value into all templates

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 const { generateRandomString } = require('./supportFunctions');
 
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -26,12 +29,16 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase}
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  }
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new",(req, res) => {
-  res.render("urls_new")
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -53,7 +60,10 @@ app.post("/login", (req, res) => {
 })
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
