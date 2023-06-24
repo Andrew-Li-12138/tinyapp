1f89861da9ad7b1dad59a04c2b3e597b67b78c47
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-24 19:07:18 -0400

    added route to handle post request for /login and set input value o cookie

--- express_server.js
+++ express_server.js
@@ -45,6 +45,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randomString}`);
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username
+  console.log(username)
+  res.cookie('username', `${username}`)
+  res.redirect('/urls')
+})
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
