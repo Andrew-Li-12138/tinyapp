66e7fa185f6839b219e81b9338a7ee9fb474c0ae
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-24 21:49:32 -0400

    add post route /logout to handle log out post request by clearing cookie

--- express_server.js
+++ express_server.js
@@ -59,6 +59,11 @@ app.post("/login", (req, res) => {
   res.redirect('/urls')
 })
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect("urls/")
+})
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, 
     longURL: urlDatabase[req.params.id],
