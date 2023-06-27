ce817c68d46cc5fb2aeacfee976a1159a29a004d
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-26 21:59:45 -0400

    add logics about only display urls if the user is logged in, only show urls that belong to the user when logged in, prompt user to register or login first, and updated /edit and /delete endpoints so only owners/creators can edit and delete links

--- express_server.js
+++ express_server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require('cookie-parser');
-const { generateRandomString, userLookupByEmail } = require('./supportFunctions');
+const { generateRandomString, userLookupByEmail, urlsForUser } = require('./supportFunctions');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-}; // JSON data
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
 
 const users = {
   aa: {
@@ -41,21 +47,34 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
 app.get("/urls", (req, res) => {
   const userID = req.cookies["user_id"];
+  console.log(userID)
+  const loginURL = urlsForUser(userID, urlDatabase)
+  console.log(loginURL)
   const templateVars = {
-    urls: urlDatabase,
+    urls: loginURL,
     user: users[userID]
   };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);// Log the POST request body to the console
+  //If the user is not logged in, respond with an HTML message that tells the user why they cannot shorten URLs.
+  const userID = req.cookies["user_id"];
+  const templateVars = { user: users[userID] }
+  if(!templateVars.user){
+    res.status(403).send("You need to login first")
+    return
+  }
+
   //add key value pair - randomString : newly entered long url - to urlDatabse
   const longURL = req.body.longURL.trim(); //delete space if there is any before/after entered URL to avoid errors in future redirecting
   const randomString = generateRandomString(6);
-  urlDatabase[randomString] = longURL;
+  urlDatabase[randomString] = {};
+  urlDatabase[randomString]['longURL'] = longURL;
+  urlDatabase[randomString]['userID'] = userID
   console.log(urlDatabase);
   //redirect to urls/:id after form submission
   res.redirect(`/urls/${randomString}`);
@@ -64,14 +83,20 @@ app.post("/urls", (req, res) => {
 app.get("/urls/new",(req, res) => {
   const userID = req.cookies["user_id"];
   const templateVars = { user: users[userID] };
+  //If the user is not logged in, redirect to GET /login
+  if(!templateVars.user){
+    res.redirect("/login")
+  }
   res.render("urls_new", templateVars);
 });
 
 app.get("/login", (req, res) => {
   const userID = req.cookies["user_id"];
-  const templateVars = {
-    user: users[userID]
-  };
+  const templateVars = { user: users[userID] };
+  //If the user is logged in, redirect to GET /urls
+  //if(templateVars.user){
+  // res.redirect("/urls")
+  //} This logic is handled in login.ejs by showing "already logged in" message and a link back to /urls
   res.render("login", templateVars);
 });
 
@@ -107,7 +132,13 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+  const userID = req.cookies["user_id"];
+  const templateVars = { user: users[userID]};
+  //If the user is logged in, redirect to GET /urls
+  //if(templateVars.user){
+   // res.redirect("/urls")
+   //}  This logic is handled in registered.ejs by showing "already logged in" message and a link back to /urls
+  res.render("register", templateVars);
 });
 
 app.post("/register", (req, res) => {
@@ -136,9 +167,26 @@ app.post("/register", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const userID = req.cookies["user_id"];
+  const userData = urlDatabase[req.params.id]
+   //If a user tries to access a shortened url that does not exist we should send them a relevant message
+   if(!userData) {
+    res.status(404).send("This short URL doesn't exist in database")
+    return
+  }
+  //The individual URL pages should not be accessible to users who are not logged in
+  if(!userID) {
+    res.status(403).send("Avaialble for logged in users only");
+    return
+  }
+  //The individual URL pages should not be accesible if the URL does not belong to them
+  if(userData['userID'] !== userID){
+    res.status(403).send("Not available for your access")
+    return
+  }
+
   const templateVars = {
     id: req.params.id,
-    longURL: urlDatabase[req.params.id],
+    longURL: userData['longURL'],
     user: users[userID]
   };
   res.render("urls_show", templateVars);
@@ -146,24 +194,67 @@ app.get("/urls/:id", (req, res) => {
 
 
 app.post("/urls/:id", (req, res) => {
+  const userID = req.cookies["user_id"]
+  const userData = urlDatabase[req.params.id]
+   //If a user tries to access a shortened url that does not exist we should send them a relevant message
+   if(!userData) {
+    res.status(404).send("This short URL doesn't exist in database")
+    return
+  }
+  //The eidt page should not be accessible to users who are not logged in
+  if(!userID) {
+    res.status(403).send("Cannot edit if not logged in");
+    return
+  }
+  //The eidt page should not be accesible if the URL does not belong to them
+  if(userData['userID'] !== userID){
+    res.status(403).send("Not available for your access")
+    return
+  }
   //take long url entered from uls_show form
   const newURL = req.body.newURL.trim();
   //substitue old long url with new url with same id (value in params)
-  urlDatabase[req.params.id] = newURL;
+  urlDatabase[req.params.id] = {} ;
+  urlDatabase[req.params.id]['longURL'] = newURL;
+  urlDatabase[req.params.id]['userID'] = userID;
   console.log(urlDatabase);
   //redirect to /urls after form submission
   res.redirect('/urls');
 });
 
 app.post("/urls/:id/delete", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const userData = urlDatabase[req.params.id]
+    //If a user tries to access a shortened url that does not exist we should send them a relevant message
+    if(!userData) {
+      res.status(404).send("This short URL doesn't exist in database")
+      return
+    }
+  //The delete page should not be accessible to users who are not logged in
+  if(!userID) {
+    res.status(403).send("Cannot delete if not logged in");
+    return
+  }
+  //The delete page should not be accesible if the URL does not belong to them
+  if(userData['userID'] !== userID){
+    res.status(403).send("Not available for your access")
+    return
+  }
+
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
 // when click at shortened url from /urls/:id page (/u/:id link), redirect to its longURL address
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  console.log(longURL);
+  const userData = urlDatabase[req.params.id];
+  const longURL = urlDatabase[req.params.id]['longURL']
+  //If a user tries to access a shortened url that does not exist we should send them a relevant message
+  if(!userData) {
+    res.status(404).send("This short URL doesn't exist in database")
+    return
+  }
+  console.log(userID);
   res.redirect(longURL);
 });
 
